d05e83966506e06c405462fdf44d51d10771c315
Gigi <n.gigi@ludus-academie.com>
2019-12-15 20:15:02 +0100

    Initial commit

---
+++ Assets/Script/CameraControler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+
+    [SerializeField] Vector2 mouseSpeed;
+    [SerializeField] float limitAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Mouse X"); //Récupère le mouvement horizontal de la souris
+        float vAxis = Input.GetAxis("Mouse Y"); //Récupère le mouvement vertical de la souris
+
+        if (hAxis!=0||vAxis!=0) //Si le joueur a bougé la souris
+        {
+            transform.Rotate(-mouseSpeed.x * vAxis*Time.deltaTime, mouseSpeed.y*hAxis*Time.deltaTime,0);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        rotation.z = 0;
+        if(rotation.x > limitAngle)
+        {
+            if(rotation.x < 180)
+            {
+                rotation.x = limitAngle;
+            }
+            else if(rotation.x < 360 - limitAngle)
+            {
+                rotation.x = 360 - limitAngle;
+            }
+        }
+        transform.eulerAngles = rotation;
+    }
+}
---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    Camera cam;
+    Rigidbody rb;
+
+    bool isGrounded = false;
+    bool jump = false;
+
+    [SerializeField] float distance;
+    [SerializeField] LayerMask layers;
+
+    [SerializeField] float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>(); //Récupère la camera
+        rb = GetComponent<Rigidbody>(); //Récupère le Rigidbody
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float yMovement = Input.GetAxis("Vertical");
+
+        if (xMovement != 0 || yMovement != 0) //Si le joueur doit bouger
+        {
+
+        }
+
+        if (Input.GetButton("Jump")&&isGrounded)
+        {
+            jump = true; //Dit que le joueur doit sauter
+        }
+    }
+
+    void FixedUpdate() //Actualisation du modèle physique
+    {
+        isGrounded = Physics.Raycast(transform.position, -transform.up,distance,layers);
+
+        if (jump && isGrounded) //Si le joueur doit sauter, il saute
+        {
+            jump = false;
+            rb.AddForce(transform.up * jumpForce); //Donne une impulsion vers le haut au joueur
+        }
+    }
+}
